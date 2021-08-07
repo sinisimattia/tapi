f88448989d188940ea44d65e2554ad422f6fb90c
Mattia Sinisi <mattia@snisni.it>
2021-08-07 10:04:31 +0200

    Better test description

--- tests/Builder.test.ts
+++ tests/Builder.test.ts
@@ -1,14 +1,16 @@
 import TestClass from '@/TestClass';
 import Builder from '@/Builder';
 
-test('Extra params are ignored', () => {
-	let json = {
-		param1: "ok",
-		param2: "ok",
-		extraParam: "not ok! abort. ABORT!"
-	}
-
-	let instance = Builder.fromJSON(new TestClass(), json);
-
-	expect(instance.param1).toBe("ok");
+describe('Typed object builder', () => {
+	test('ignores extra parameters', () => {
+		let json = {
+			param1: "ok",
+			param2: "ok",
+			extraParam: "not ok! abort. ABORT!"
+		}
+	
+		let instance = Builder.fromJSON(new TestClass(), json);
+	
+		expect(instance.param1).toBe("ok");
+	})
 })

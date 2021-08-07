4ad20fbe55a7e565ee09d2fe4182a1c4b935cb03
Mattia Sinisi <mattia@snisni.it>
2021-08-07 17:06:20 +0200

    Added "strict" assignment mode

--- src/Builder.ts
+++ src/Builder.ts
@@ -1,11 +1,11 @@
 import Describer from "@/helpers/Describer";
 
 export default class Builder {
-	public static fromJSON<ResultType>(target: ResultType, json: any): ResultType {
+	public static fromJSON<ResultType>(target: ResultType, json: any, strict: boolean = false): ResultType {
 		const params = Describer.getParameters(target);
 
 		params.forEach(param => {
-			if (!json.hasOwnProperty(param)) {
+			if (strict && !json.hasOwnProperty(param)) {
 				throw new Error("Invalid input object, missing parameter: " + param);
 			}
 			

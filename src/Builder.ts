a6c6680ada0d72ba46f4868230a836f855672713
Mattia Sinisi <mattia@snisni.it>
2021-08-07 17:37:15 +0200

    Builder class now created with factory pattern to allow further customization

--- src/Builder.ts
+++ src/Builder.ts
@@ -1,10 +1,21 @@
 import Describer from "@/helpers/Describer";
 
-export default class Builder {
-	public static fromJSON<ResultType>(target: ResultType, json: any, strict: boolean = false): ResultType {
+export default class Builder<ResultType> {
+	private ignores: string[] = [];
+
+	public ignore(paths: string[]): this {
+		this.ignores = paths;
+		return this;
+	}
+
+	public fromJSON(target: ResultType, json: any, strict: boolean = false): ResultType {
 		const params = Describer.getParameters(target);
 
 		params.forEach(param => {
+			if (this.ignores.includes(param)) {
+				return;
+			}
+
 			if (!json.hasOwnProperty(param)) {
 				if (strict) {
 					throw new Error("Invalid input object, missing parameter: " + param);

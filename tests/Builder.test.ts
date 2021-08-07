b35c86123badf552277dd83e95fcf296b2e20222
Mattia Sinisi <mattia@snisni.it>
2021-08-07 17:06:28 +0200

    Updated tests

--- tests/Builder.test.ts
+++ tests/Builder.test.ts
@@ -1,16 +1,30 @@
-import TestClass from '@/TestClass';
 import Builder from '@/Builder';
+
+class TestClass {
+	public param1: string = "yo";
+	private param2: string = "there is no such thing as privacy"
+}
 
 describe('Typed object builder', () => {
 	test('ignores extra parameters', () => {
 		let json = {
 			param1: "ok",
-			param2: "ok",
 			extraParam: "not ok! abort. ABORT!"
 		}
 	
 		let instance = Builder.fromJSON(new TestClass(), json);
 	
 		expect(instance.param1).toBe("ok");
+	})
+
+	test('enforces strict assignment', () => {
+		let json = {
+			param1: "ok",
+			extraParam: "not ok! abort. ABORT!"
+		}
+
+		expect(() => {
+			Builder.fromJSON(new TestClass(), json, true);
+		}).toThrowError();
 	})
 })

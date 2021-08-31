b661a2d34665f5a1f3d983930c7c3313eb1e6cdf
Mattia Sinisi <m.sinisi@key2.it>
2021-08-31 15:04:31 +0200

    Added version number to thank you script

--- install/thankyou.js
+++ install/thankyou.js
@@ -1,7 +1,10 @@
 const { colored } = require('./consolehelpers')
+const package = require('../package.json')
+
+const message = "Thanks for using tapi!"
+const version = colored("v", ) + package.version
 
 const initialPadding = ' '.repeat(6)
-const message = "Thanks for using tapi!"
 const topBorder = '╔' + '═'.repeat(message.length + 2) + '╗'
 const bottomBorder = '╚' + '═'.repeat(message.length + 2) + '╝'
 const emptySection = '║ ' + ' '.repeat(message.length) + ' ║'
@@ -11,3 +14,4 @@ console.log(initialPadding + colored(emptySection));
 console.log(initialPadding + colored('║ ') + message + colored(' ║'));
 console.log(initialPadding + colored(emptySection));
 console.log(initialPadding + colored(bottomBorder));
+console.log(initialPadding + ' '.repeat(message.length + 4 - version.length) + version)

1bc53087594a772295fa3d3f2cd0e639f8f79ce7
Mattia Sinisi <m.sinisi@key2.it>
2021-08-31 14:57:36 +0200

    Code cleanup for thank you script

--- install/thankyou.js
+++ install/thankyou.js
@@ -2,12 +2,12 @@ const { colored } = require('./consolehelpers')
 
 const initialPadding = ' '.repeat(6)
 const message = "Thanks for using tapi!"
-const topBorder = initialPadding + '╔' + '═'.repeat(message.length + 2) + '╗'
-const bottomBorder = initialPadding + '╚' + '═'.repeat(message.length + 2) + '╝'
-const emptySection = initialPadding + '║ ' + ' '.repeat(message.length) + ' ║'
+const topBorder = '╔' + '═'.repeat(message.length + 2) + '╗'
+const bottomBorder = '╚' + '═'.repeat(message.length + 2) + '╝'
+const emptySection = '║ ' + ' '.repeat(message.length) + ' ║'
 
-console.log(colored(topBorder));
-console.log(colored(emptySection));
+console.log(initialPadding + colored(topBorder));
+console.log(initialPadding + colored(emptySection));
 console.log(initialPadding + colored('║ ') + message + colored(' ║'));
-console.log(colored(emptySection));
-console.log(colored(bottomBorder));
+console.log(initialPadding + colored(emptySection));
+console.log(initialPadding + colored(bottomBorder));

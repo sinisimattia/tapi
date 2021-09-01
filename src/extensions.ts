c39baf22af817f008080b63ff93194dd1260a61f
Mattia Sinisi <m.sinisi@key2.it>
2021-09-01 17:17:44 +0200

    Added the option to select only a specific field

--- src/extensions.ts
+++ src/extensions.ts
@@ -4,15 +4,24 @@ import ResourceFactory from '@/contracts/ResourceFactory';
 declare global {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	export interface Promise<T> {
-		as<T>(classToBuild: ResourceFactory<T>): Promise<T>;
+		as<T>(classToBuild: ResourceFactory<T>, from: string): Promise<T>;
 	}
 }
 
-Promise.prototype.as = function<T> (classToBuild: ResourceFactory<T>): Promise<T> {
-	return this.then(input => {
+Promise.prototype.as = function<T> (classToBuild: ResourceFactory<T>, from: string = ''): Promise<T> {
+	return this.then((input: any) => {
 		return new Promise((resolve, reject) => {
 			let result;
 			try {
+				if (from != '') {
+					if (input.hasOwnProperty(from)) {
+						input = input[from];
+					}
+					else {
+						throw new Error(`Incoming object does not contain a field called "${input}".`);
+					}
+				}
+
 				result = new Builder(classToBuild).fromJSON(input)
 				resolve(result);
 			}

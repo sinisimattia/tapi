81ba06c00548a0dd8da5fce4eab0aa979ff442be
Mattia Sinisi <mattia@snisni.it>
2021-09-03 21:11:54 +0200

    WIP: fixed decorator issues

--- src/decorators.ts
+++ src/decorators.ts
@@ -2,17 +2,17 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 //FIXME Remove these ☝ and put them in eslintrc
 
-export function Resource(targetConstructor: Function) {
+export function Resource(targetConstructor: Function): any {
 	console.log("Resource: ", targetConstructor)
 }
 
-export function Prop() {
+export function Prop(): any {
 	return (target: any, name?: PropertyKey): any => {
 		console.log("Prop: ", name, target)
 	};
 }
 
-export function Transform() {
+export function Transform(): any {
 	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 		console.log("Transform: ", target, propertyKey, descriptor)
 	};

735271577001f50eeaea34a2d88c5615b31e68fe
Mattia Sinisi <mattia@snisni.it>
2021-09-05 14:43:29 +0200

    WIP: Get the same instanche of the builder in every decorator

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,15 +1,36 @@
-export function Resource(targetConstructor: Function): any {
-	console.log("Resource: ", targetConstructor)
+import Builder from "@/Builder";
+import BuildableResource from "@/contracts/BuildableResource";
+
+class ResourceDecorator {
+	public Resource(constructor: Function): any {
+		const target = constructor.prototype;
+
+		if (target instanceof BuildableResource) {
+			const builder = new Builder<typeof target>(target);
+			target.newBuilder = builder;
+		}
+		else {
+			throw new Error("Target class has to extend BuildableResource in order to use this decorator.");
+		}
+	}
+
+	public Prop(): any {
+		return (target: any, name?: PropertyKey): any => {
+			console.log(target, name);
+		};
+	}
+
+	public Transform(): any {
+		return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+			console.log(target, propertyKey, descriptor);
+		};
+	}
 }
 
-export function Prop(): any {
-	return (target: any, name?: PropertyKey): any => {
-		console.log("Prop: ", name, target)
-	};
-}
+const resourceDecorator = new ResourceDecorator();
 
-export function Transform(): any {
-	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-		console.log("Transform: ", target, propertyKey, descriptor)
-	};
-}
+export const Resource = resourceDecorator.Resource; 
+
+export const Prop = resourceDecorator.Prop; 
+
+export const Transform = resourceDecorator.Transform; 

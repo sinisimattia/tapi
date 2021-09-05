0f703ad6f5302c33f971cc270e933ec63ed0cfa1
Mattia Sinisi <mattia@snisni.it>
2021-09-05 14:43:08 +0200

    Getter / Setter for builder property in BuildableResource

--- src/contracts/BuildableResource.ts
+++ src/contracts/BuildableResource.ts
@@ -1,3 +1,16 @@
+import Builder from "@/Builder";
+
 export default class BuildableResource {
+	private builder?: Builder<this>;
+
+	public get currentBuilder(): Builder<this> | undefined {
+		return this.builder;
+	}
+
+	public set newBuilder(builder: Builder<this>) {
+		this.builder = builder;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	build(source: any): any {}
 }

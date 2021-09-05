ae6ec96603c352fa00373c43d48e335be6e98af3
Mattia Sinisi <mattia@snisni.it>
2021-09-05 18:59:43 +0200

    Played around with decorators

--- src/demo.ts
+++ src/demo.ts
@@ -1,13 +1,18 @@
-import { Resource, Prop, Transform } from '@/decorators';
+import { Resource, Alias, Transform, Ignore } from '@/decorators';
 import BuildableResource from '@/contracts/BuildableResource';
 
 @Resource
 class TestClass extends BuildableResource {
-	@Prop()
+	@Alias("aliasForThing")
 	public thing: string = "ciao";
+
+	@Ignore
+	public thingToIgnore: string = "if you see this text everything works"
 
 	@Transform()
 	public trasformThing(): any {
 		//
 	}
 }
+
+console.log(new TestClass().currentBuilder);

de656c2dc00d352fb1b3b1882bb44357b2661686
Mattia Sinisi <mattia@snisni.it>
2021-09-05 19:13:00 +0200

    Installed Babel plugins for decorators

--- babel.config.js
+++ babel.config.js
@@ -10,4 +10,11 @@ module.exports = {
 		],
 		'@babel/preset-typescript'
 	],
+	plugins: [
+		[
+			'@babel/plugin-proposal-decorators',
+			{ legacy: true }
+		],
+		'@babel/plugin-proposal-class-properties'
+	]
 };

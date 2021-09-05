d50a6307a5172aeb8c0a42e149153dfd8c077223
Mattia Sinisi <mattia@snisni.it>
2021-09-05 18:59:33 +0200

    Implemented decorators for easier config

--- src/decorators.ts
+++ src/decorators.ts
@@ -9,12 +9,29 @@ class ResourceDecorator {
 		}
 	}
 
-	public Prop(): any {
-		return (target: BuildableResource, name?: PropertyKey): any => {};
+	public Alias(alias: string): any {
+		return (target: BuildableResource, name: PropertyKey): any => {
+			target.currentBuilder?.alias(alias, name.toString());
+		};
 	}
 
-	public Transform(): any {
-		return function (target: BuildableResource, propertyKey: string, descriptor: PropertyDescriptor) {};
+	public Transform(localPath: string = ""): any {
+		return function (target: BuildableResource, propertyKey: string, descriptor: PropertyDescriptor) {
+			if(!localPath) {
+				const p = propertyKey.replace("transform", "").toLowerCase();
+
+				if (!p) {
+					throw new Error("Method name does not begin with 'transform', please update the name or specify the property name yourself.");
+				}
+
+				localPath = p;
+			}
+			target.currentBuilder?.transform(localPath, target[propertyKey]);
+		};
+	}
+
+	public Ignore(target: BuildableResource, name: PropertyKey) {
+		target.currentBuilder?.ignore([name.toString()]);
 	}
 }
 
@@ -22,6 +39,8 @@ const resourceDecorator = new ResourceDecorator();
 
 export const Resource = resourceDecorator.Resource;
 
-export const Prop = resourceDecorator.Prop; 
+export const Alias = resourceDecorator.Alias; 
 
 export const Transform = resourceDecorator.Transform; 
+
+export const Ignore = resourceDecorator.Ignore;

3f24228daba8713aab22267162a03cb2fe358df9
Mattia Sinisi <m.sinisi@key2.it>
2021-09-18 21:10:03 +0200

    Added code docs for helpers

--- src/helpers/Describer.ts
+++ src/helpers/Describer.ts
@@ -1,4 +1,13 @@
+/**
+ * An object describer helper.
+ */
 export default class Describer {
+	/**
+	 * Retreive a list of all params of a class instance.
+	 * 
+	 * @param instance The class to analyze.
+	 * @returns A list of the class's properties.
+	 */
 	public static getParameters(instance: unknown): Array<string> {
 		return Object.getOwnPropertyNames(instance);
 	}

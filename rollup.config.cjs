646aff0fd2fa35b0db7fcf56cc155774773a3764
Mattia Sinisi <mattia@snisni.it>
2022-12-27 16:46:23 +0100

    feat(compiler): Added CJS/ES module support and tree shaking

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -4,28 +4,70 @@ const ts = require('@rollup/plugin-typescript')
 const cjs = require('@rollup/plugin-commonjs')
 const json = require('@rollup/plugin-json')
 const resolve = require('@rollup/plugin-node-resolve')
+const alias = require('@rollup/plugin-alias')
 
 const OUT_DIR = pkg.directories.lib
 
-module.exports = {
-	input: [
-		'src/index.ts',
-		'src/functions.ts',
-		'src/extensions.ts',
-		'src/decorators.ts',
+const customResolver = resolve({
+	extensions: [
+		// JavaScript
+		'.js', '.cjs', '.mjs',
+		// TypeScript
+		'.ts', '.d.ts',
+		// Other
+		'.json', '.node',
 	],
-	output:[
-		{
-			dir: OUT_DIR,
-			format: 'es'
-		},
-	],
-	plugins: [
-		resolve(),
-		cjs(),
-		json(),
-		ts({
-			tsconfig: './tsconfig.build.json'
-		}),
-	]
-}
+})
+
+module.exports = [
+	// ES Module build
+	{
+		input: [
+			'src/index.ts',
+			'src/functions.ts',
+			'src/extensions.ts',
+			'src/decorators.ts',
+		],
+		output:[
+			{
+				dir: OUT_DIR + '/es',
+				format: 'es'
+			},
+		],
+		plugins: [
+			alias({customResolver}),
+			cjs(),
+			json(),
+			ts({
+				tsconfig: './tsconfig.build.json',
+				outDir: OUT_DIR + '/es',
+				tsBuildInfoFile: OUT_DIR + '/es/' + '.tsbuildinfo'
+			}),
+		],
+	},
+	// CommonJS module build
+	{
+		input: [
+			'src/index.ts',
+			'src/functions.ts',
+			'src/extensions.ts',
+			'src/decorators.ts',
+		],
+		output:[
+			{
+				dir: OUT_DIR + '/cjs',
+				format: 'cjs'
+			},
+		],
+		plugins: [
+			alias({customResolver}),
+			cjs(),
+			json(),
+			ts({
+				tsconfig: './tsconfig.build.json',
+				outDir: OUT_DIR + '/cjs',
+				tsBuildInfoFile: OUT_DIR + '/cjs/' + '.tsbuildinfo'
+			}),
+		],
+	},
+]

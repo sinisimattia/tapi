a4a92bd81081648f7cf0cd07ec2deff4865d955d
Mattia Sinisi <mattia@snisni.it>
2022-12-27 17:30:10 +0100

    fix(compile): Cleaned up build config

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,4 +1,3 @@
-const pkg = require('./package.json')
 // Plugins
 const ts = require('@rollup/plugin-typescript')
 const cjs = require('@rollup/plugin-commonjs')
@@ -6,68 +5,49 @@ const json = require('@rollup/plugin-json')
 const resolve = require('@rollup/plugin-node-resolve')
 const alias = require('@rollup/plugin-alias')
 
+// Constants
+const pkg = require('./package.json')
 const OUT_DIR = pkg.directories.lib
 
-const customResolver = resolve({
-	extensions: [
-		// JavaScript
-		'.js', '.cjs', '.mjs',
-		// TypeScript
-		'.ts', '.d.ts',
-		// Other
-		'.json', '.node',
-	],
-})
+function createBuildConfig(outFormat, outSubDir) {
+	return {
+		input: [
+			'src/index.ts',
+			'src/functions.ts',
+			'src/extensions.ts',
+			'src/decorators.ts',
+		],
+		output:[
+			{
+				dir: OUT_DIR + '/' + outSubDir,
+				format: outFormat
+			},
+		],
+		plugins: [
+			alias({
+				customResolver: resolve({
+					extensions: [
+						// JavaScript
+						'.js', '.cjs', '.mjs',
+						// TypeScript
+						'.ts', '.d.ts',
+						// Other
+						'.json', '.node',
+					],
+				}),
+			}),
+			cjs(),
+			json(),
+			ts({
+				tsconfig: './tsconfig.build.json',
+				outDir: OUT_DIR + '/' + outSubDir,
+				tsBuildInfoFile: OUT_DIR + '/' + outSubDir + '/' + '.tsbuildinfo'
+			}),
+		],
+	}
+}
 
 module.exports = [
-	// ES Module build
-	{
-		input: [
-			'src/index.ts',
-			'src/functions.ts',
-			'src/extensions.ts',
-			'src/decorators.ts',
-		],
-		output:[
-			{
-				dir: OUT_DIR + '/es',
-				format: 'es'
-			},
-		],
-		plugins: [
-			alias({customResolver}),
-			cjs(),
-			json(),
-			ts({
-				tsconfig: './tsconfig.build.json',
-				outDir: OUT_DIR + '/es',
-				tsBuildInfoFile: OUT_DIR + '/es/' + '.tsbuildinfo'
-			}),
-		],
-	},
-	// CommonJS module build
-	{
-		input: [
-			'src/index.ts',
-			'src/functions.ts',
-			'src/extensions.ts',
-			'src/decorators.ts',
-		],
-		output:[
-			{
-				dir: OUT_DIR + '/cjs',
-				format: 'cjs'
-			},
-		],
-		plugins: [
-			alias({customResolver}),
-			cjs(),
-			json(),
-			ts({
-				tsconfig: './tsconfig.build.json',
-				outDir: OUT_DIR + '/cjs',
-				tsBuildInfoFile: OUT_DIR + '/cjs/' + '.tsbuildinfo'
-			}),
-		],
-	},
+	createBuildConfig('es', 'es'),
+	createBuildConfig('cjs', 'cjs')
 ]

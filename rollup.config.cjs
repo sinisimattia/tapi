48e24208a25176a092e8ffbd364257fe4c811d53
Mattia Sinisi <mattia@snisni.it>
2023-04-15 01:43:19 +0200

    Clear files after build

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -5,12 +5,14 @@ const cjs = require('@rollup/plugin-commonjs')
 const json = require('@rollup/plugin-json')
 const resolve = require('@rollup/plugin-node-resolve')
 const alias = require('@rollup/plugin-alias')
+const clear = require('rollup-plugin-clear')
 
 // Constants
 const pkg = require('./package.json')
+const LIB_DIR = pkg.directories.lib
 
 function createBuildConfig(outSubDir, outFormat) {
-	const OUT_DIR = pkg.directories.lib + '/' + outSubDir
+	const OUT_DIR = LIB_DIR + '/' + outSubDir
 
 	return {
 		input: [
@@ -42,8 +44,8 @@ function createBuildConfig(outSubDir, outFormat) {
 			json(),
 			ts({
 				tsconfig: './tsconfig.build.json',
-				declarationDir: OUT_DIR + '/types',
 				outDir: OUT_DIR,
+				declarationDir: OUT_DIR + '/types',
 			}),
 		],
 	}
@@ -53,14 +55,22 @@ module.exports = [
 	createBuildConfig('cjs', 'cjs'),
 	createBuildConfig('es', 'es'),
 	{
-		input: 'dist/es/types/index.d.ts',
-		output: [{ file: 'dist/types.d.ts', format: 'es' }],
+		input: LIB_DIR + '/es/types/index.d.ts',
+		output: [{ file: LIB_DIR + '/types.d.ts', format: 'es' }],
 		plugins: [
 			dts.default({
 				tsconfig: './tsconfig.build.json',
 				compilerOptions: {
-					baseUrl: 'src'
+					baseUrl: 'src',
+					sourceMap: true,
+					declarationMap: true,
 				},
+			}),
+			clear({
+				targets: [
+					LIB_DIR + '/es/types',
+					LIB_DIR + '/cjs/types',
+				],
 			}),
 		],
 	},
